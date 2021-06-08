5f78b893f40816708bf35e2d8fbafd7c5244afec
Basil Eagle <basileagle@gmail.com>
2021-06-08 16:19:58 +0100

    angular material init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- server/index.js
+++ server/index.js
@@ -1,9 +1,66 @@
 const http = require('http');
 const express = require('express');
+const mysql = require('mysql');
+require('dotenv/config');
+
+const usersRoute = require('./routes/users');
+
+const db = mysql.createConnection({
+    host        : process.env.HOST || 'localhost',
+    user        : 'root',
+    password    : process.env.PASSWORD,
+    database    : 'activitylogger'
+});
+
+function checkOrCreateDb() {
+    const sql = 'CREATE DATABASE IF NOT EXISTS activitylogger;';
+    db.query(sql, (err, _result) => {
+        if (err) throw err;
+        console.log("Checking database...");
+    });
+}
+
+function checkOrCreateTables() {
+    const sql = [`CREATE TABLE IF NOT EXISTS users(
+        userID int AUTO_INCREMENT, 
+        username VARCHAR(255) NOT NULL, 
+        password VARCHAR(255) NOT NULL,
+        PRIMARY KEY (userID));`,
+    `CREATE TABLE IF NOT EXISTS tasks(
+        taskID int AUTO_INCREMENT, 
+        userID int NOT NULL, 
+        date datetime NOT NULL,
+        state tinyint NOT NULL,
+        notes VARCHAR(255),
+        PRIMARY KEY (taskID),
+        FOREIGN KEY (userID) REFERENCES users(userID));`];
+    sql.forEach(query => {
+        db.query(query, (err, _result) => {
+            if (err) throw err;
+        });
+    });
+    console.log("Checking tables...");
+}
+
+db.connect((err) => {
+    if (err) throw err;
+    checkOrCreateDb();
+    checkOrCreateTables();
+    console.log('Database connected...');
+});
 
 const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
+
+app.use('/users', usersRoute);
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 server.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
