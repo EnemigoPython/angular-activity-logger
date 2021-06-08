8b2587b851229699a9bb6988cec86eece2b5f547
Basil Eagle <basileagle@gmail.com>
2021-06-08 17:11:36 +0100

    buttons

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { HeaderComponent } from './components/header/header.component';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
