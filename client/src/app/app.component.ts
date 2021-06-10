a08b2a6ac8cfb38f428bf58d5e157df499c26496
Basil Eagle <basileagle@gmail.com>
2021-06-10 17:00:29 +0100

    current user

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // todo: local storage token to identify user on init
-  private user: string | null = null;
 }
--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 import {LoginDetails} from '../LoginDetails';
 
@@ -16,16 +16,23 @@ const httpOptions = {
 })
 export class AccountService {
   private apiUrl = environment.API_URL;
+  private currentUser: string | null = null;
+  private subject = new Subject<any>();
 
   constructor(
     private http: HttpClient
   ) { }
+
+  setCurrentUser(newUser: string) {
+    this.currentUser = newUser;
+    this.subject.next(this.currentUser);
+  }
+
   requestLogin(details: LoginDetails): void {
 
   }
+
   requestNewAccount(details: LoginDetails): Observable<LoginDetails> {
-    console.log('hello');
-    console.log(environment.API_URL);
     return this.http.get<LoginDetails>(this.apiUrl);
     // return this.http.post<LoginDetails>(this.apiUrl, details, httpOptions);
   }
