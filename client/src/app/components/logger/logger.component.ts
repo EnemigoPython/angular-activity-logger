5067c50129c2af6baa0c866928d29feb9fd27595
Basil Eagle <basileagle@gmail.com>
2021-07-01 16:45:11 +0100

    dialog position

--- client/src/app/components/logger/logger.component.ts
+++ client/src/app/components/logger/logger.component.ts
@@ -11,11 +11,11 @@ import { ActivitiesService } from 'src/app/services/activities.service';
 import { ActivityRow } from '../../types/ActivityRow';
 
 const currentDate = new Date()
-.toLocaleString('en-GB', {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-});
+  .toLocaleString('en-GB', {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
 
 @Component({
   selector: 'app-logger',
@@ -41,31 +41,31 @@ export class LoggerComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.accountService.observerID()
-    .subscribe(
-      id => {
-        if (id > 0 && id !== this.currentID) {
-          this.currentID = id;
-          this.activitiesService.getUserActivities(id)
-          .subscribe(
-            data => {
-              if (data.length > 0) {
-                const absentDaysCount = this.activitiesService.countAbsentDays(data[data.length - 1].activityDate);
-                if (absentDaysCount > 0) {
-                  this.activitiesService.updateRecentDates(absentDaysCount, id)
-                  .subscribe(newData => {
-                    this.dataSource.data = this.activitiesService.buildTableFromIndices(data.concat(newData));
-                    this.displayedColumns = Object.keys(this.dataSource.data[0]);
-                  });
-                } else {
-                  this.dataSource.data = this.activitiesService.buildTableFromIndices(data);
-                  this.displayedColumns = Object.keys(this.dataSource.data[0]);
+      .subscribe(
+        id => {
+          if (id > 0 && id !== this.currentID) {
+            this.currentID = id;
+            this.activitiesService.getUserActivities(id)
+              .subscribe(
+                data => {
+                  if (data.length > 0) {
+                    const absentDaysCount = this.activitiesService.countAbsentDays(data[data.length - 1].activityDate);
+                    if (absentDaysCount > 0) {
+                      this.activitiesService.updateRecentDates(absentDaysCount, id)
+                        .subscribe(newData => {
+                          this.dataSource.data = this.activitiesService.buildTableFromIndices(data.concat(newData));
+                          this.displayedColumns = Object.keys(this.dataSource.data[0]);
+                        });
+                    } else {
+                      this.dataSource.data = this.activitiesService.buildTableFromIndices(data);
+                      this.displayedColumns = Object.keys(this.dataSource.data[0]);
+                    }
+                  }
                 }
-              }
-            }
-          );
+              );
+          }
         }
-      }
-    );
+      );
   }
 
   selectActivity(j: number, col: string, item: string) {
@@ -77,18 +77,19 @@ export class LoggerComponent implements AfterViewInit {
       data: {
         name: col,
         id: this.activitiesService.retrieveFromIndexID(`${col}[${itemIndex}]`)
-      }
+      },
+      position: { top: "20%" }
     });
   }
 
   addActivity() {
     if (!this.displayedColumns.includes(this.activityName)) {
       this.dataSource.data = this.dataSource.data.map(row => {
-        return {...row, [this.activityName]: 'unreported'};
+        return { ...row, [this.activityName]: 'unreported' };
       });
       this.displayedColumns.push(this.activityName);
       this.activitiesService.postNewActivity(
-        this.activityName, 
+        this.activityName,
         this.dataSource.data.length,
         this.currentID!
       );
